3b0430a1de48ab4370d6993caf48e71a36d726ea
Jared Grippe <jared@deadlyicon.com>
2013-05-31 10:41:16 -0700

    setup rspec

--- config/application.rb
+++ config/application.rb
@@ -17,12 +17,14 @@ module Jaredatron
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.assets.debug = false
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+
+    config.app_generators do |c|
+      c.test_framework :rspec, :fixture => true, :fixture_replacement => nil
+      c.integration_tool :rspec
+      c.performance_tool :rspec
+    end
+
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Jaredatron::Application.config.session_store :cookie_store, key: '_focus_session'
+Jaredatron::Application.config.session_store :cookie_store, key: '_jaredatron_session'
